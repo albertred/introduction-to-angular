0f460edd0d407057c54ebae373b9eefc529da5e6
Michelle <michelle.lu187@gmail.com>
2024-05-07 23:41:42 -0400

    add simple housing component

---
+++ src/app/housing-location.ts
@@ -0,0 +1,13 @@
+export interface HousingLocation {
+	name: string,
+	city: string,
+	state: string,
+	photo: string,
+	availableUnits: number,
+	wifi: boolean;
+	laundry: boolean;
+
+}
+
+// creating an array
+
